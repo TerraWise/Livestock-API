cbc0f5a43e61d761c2b94148704eac0ca40d78c6
VTM-Planfarm <vinh@planfarm.com.au>
2026-07-13 07:26:15 +0800

    Remove `print` statement for debugging

--- internal/stock_class.py
+++ internal/stock_class.py
@@ -44,7 +44,6 @@ class Livestock:
 
     def stock_class_data(self, seasonal_data: dict):
         species_data = seasonal_data.get(self.species, {})
-        print(species_data)
         for i, id in enumerate(self.ids):
             stock_data = species_data.get(id)
             for stock_class in self.stock_classes:
