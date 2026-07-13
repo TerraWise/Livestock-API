e402d9dff3f952118206e39f32ab47f41192e622
VTM-Planfarm <vinh@planfarm.com.au>
2026-07-13 09:47:23 +0800

    Refactor vegetation_planting function to handle None values for beef and sheep proportions

--- internal/vegetation.py
+++ internal/vegetation.py
@@ -2,14 +2,18 @@ from openpyxl import Workbook
 
 
 def vegetation_planting(
-    beef_proportion: list[float] = [0],
-    sheep_proportion: list[float] = [0],
+    beef_proportion: list[float] | None = None,
+    sheep_proportion: list[float] | None = None,
     age: int = 0,
     area: float = 0,
     region: str = "South West",
     soil: str = "Loams & Clays",
     tree_species: str = "Mixed species (Environmental Plantings)",
 ) -> dict:
+    if beef_proportion is None:
+        beef_proportion = [0]
+    if sheep_proportion is None:
+        sheep_proportion = [0]
     return {
         "beefProportion": beef_proportion,
         "sheepProportion": sheep_proportion,
@@ -27,14 +31,10 @@ def extract_veg_data(inventory_sheet: Workbook) -> dict:
     ws = inventory_sheet["🌿 Vegetation"]
     veg_data = {"vegetation": []}
 
-    for i, row in enumerate(
-        ws.iter_rows(min_row=2, min_col=2, max_col=8, values_only=True)
-    ):
+    for row in ws.iter_rows(min_row=2, min_col=2, max_col=8, values_only=True):
         region, tree_species, _, soil, area, _, age = row
 
         if any(value is None for value in (region, tree_species, soil, area, age)):
-            if i == 0:
-                veg_data["vegetation"].append(vegetation_planting())
             break
 
         veg_data["vegetation"].append(
